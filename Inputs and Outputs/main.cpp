e3b10a3d8e2b7da4e8dd4bd117a73b2c9428a854
Juanc179 <jcpisacane@hotmail.com>
2024-03-05 17:42:19 -0300

    inputs and outputs

--- Inputs and Outputs/main.cpp
+++ Inputs and Outputs/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main(){
+    int age;
+    std::string name;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "I am " << 13 << " years old" << std::endl;
+    std::cout << "How is your name?¿How old are you? " << std::endl;
+    std::cin >> name >> age;
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
+
+    std::cerr << "Error Message: Something is wrong" << std::endl;
+    std::clog << "Log Message: Something Happened" << std::endl;
 
     return 0;
 }
