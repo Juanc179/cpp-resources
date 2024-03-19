8fd5addb656134f83e989db8e7ef01e2c7885299
Juanc179 <jcpisacane@hotmail.com>
2024-03-19 16:57:13 -0300

    Hour 6:33

---
+++ Output formatting/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <ios>
+#include <iomanip>
+
+/*
+
+can be disabled with no---.
+std::endl -> gives a enter to the text, also with /n.
+std::flush ->  sends data stored in buffer to the terminal.
+boolalpha -> bool output format.
+setfill() -> sets a fill for the given with of a text
+left -> justification
+right -> justification
+setw() -> specifies the width for any text. between de parentheses goes the width.
+hex |
+dec | -> change the base of the number on the output.
+oct |
+showbase -> shows the base the number is output
+fixed 
+setprecision() - > decimal positions after the decimal points. limited by the type of variable.
+showpoint -> forces the decimal point being shown.
+scientific -> uses exponential notation when necessary
+uppercase -> uppercase shows the data on uppercase
+showpos -> shows the positive sign in a positive number
+*/
+int main(){
+
+    return 0;
+}
