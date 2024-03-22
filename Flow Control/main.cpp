679ac4d23b9d677ddfa5a5d4fb55202c894a1ac4
Juanc179 <jcpisacane@hotmail.com>
2024-03-22 19:25:17 -0300

    hora 7:50

---
+++ Flow Control/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main(){
+
+    return 0;
+}
+
+/*
+Flow Control.
+
+If 
+    If(Condition){ if condition is empty, only a variable, enters if TRUE or 0.
+        body
+    } else{ // secondary condition
+        
+    }
+    with else if you cant concanate multiple conditions. 
+    another way to ask for false
+    if(!(result==true))
+    another way to ask for a bool
+    bool variable = (num < num1) true or false depending of the output of the condition. 
+
+switch ( variable ){
+
+    case [condition]: {
+        consequence; break; // break jumps out of the switch statement. if not present it will execute all the consequences.
+
+    }
+    case condition2 : {
+        break;
+    }
+    case condition3: // if condition 3 is right, the code will execute until condition5 
+    case condition4:
+    case condition5:{
+        break;
+    }
+
+    default : break; // default condition if none is correct.
+}
+
+ternary operator
+    result = (condition) ? option1 : option 2; // option must be the same type or casted to the same type
+    is the same as 
+    if(condition){
+        result = option1; // result becomes equal to option1. 
+    } else{
+        result = option2;
+    }
+*/
--- basic operations/main.cpp
+++ basic operations/main.cpp
@@ -5,3 +5,35 @@ int main(){ // entry point. first thing to run.
     std::cout<< "Hello World" << std::endl;
     return 0;
 }
+/*
+std::alphatype
+
+Basic operations
+add, + sum
+substract, - minus
+divide, / divides..
+modulus, % gives the rest of the division 
+multiply, * multiplies..
+
+associativity and precedence
+most of all run like you read, others run from right to left. there is a specific precedence table for g++
+multipling and division happen first than addition and substraction. parenthesis can change this. 
+
+postfix value++, if you try to print value++, it will print the previous value and in the next use of the value, it will be a +1. same happens when substracting with --
+prefix --value, same but applies in the first use, if in print you call ++value, in that print it will be added 1.
+
+
+Math operations <cmath>
+std::...
+floor() -> round down
+ceil() -> round up.
+abs() -> absolute
+cos()
+sin()
+tan()
+log()
+pow()
+sqrt()
+round() -> rounds to nearest?
+
+*/
