8d4eab779da16b89156c1a8e8b01e779795b3bb5
Juanc179 <jcpisacane@hotmail.com>
2024-04-08 17:07:31 -0300

    arrays and loops, hour 9:25

--- Loops/main.cpp
+++ Loops/main.cpp
@@ -22,4 +22,44 @@ body, inside curly braces, code that will wxecute multiple parts.
 
 size_t is not a type, just a alias for some unsigned int representation. 8 bit unsigned int.
 
+the variables declared inside de for loop are not usable outside of it.
+
+range based for loops
+
+for ( range_declaration : range_expression ) 
+    loop_statement
+Parameters :
+range_declaration : 
+a declaration of a named variable, whose type is the 
+type of the element of the sequence represented by 
+range_expression, or a reference to that type.
+Often uses the auto specifier for automatic type 
+deduction. size of the array
+range_expression : 
+any expression that represents a suitable sequence 
+or a braced-init-list. as an array
+loop_statement : 
+any statement, typically a compound statement, which
+is the body of the loop.
+source: https://www.geeksforgeeks.org/range-based-loop-c/
+
+
+While Loops.
+
+const unsigned int count {10};
+
+unsigned int i {0}; // iterator declared outside
+
+while ( i < count){ //test, continues if true, only enters if true.
+    //shall be done
+    i++; // incrementation mark
+}
+
+Do while loop
+
+do{
+
+
+
+}while (test); // will be done at least one time, until test is false
 */
